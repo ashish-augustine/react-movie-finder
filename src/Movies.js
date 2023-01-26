20a0bbe720a25354105d927f10ec671240522b78
Ashish Augustine <ashish.augustine123@gmail.com>
2023-01-26 01:03:32 +0100

    first commit

---
+++ src/Movies.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useGlobalContext1 } from "./context";
+import { Link } from "react-router-dom";
+const url =
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
+
+const Movies = () => {
+  const { movies, isLoading } = useGlobalContext1();
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="movies">
+      {movies.map((movie) => {
+        const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
+        {
+          /*  Line 16: The second ones are the programer defined ones and the first ones the real from json query*/
+        }
+        return (
+          <Link to={`/movies/${id}`} key={id} className="movie">
+            <article>
+              <img src={poster === "N/A" ? url : poster} alt={title} />
+              <div className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Movies;
---
+++ src/SearchForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useGlobalContext1 } from "./context";
+const SearchForm = () => {
+  const { query, setQuery, error } = useGlobalContext1();
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>search movies</h2>
+      <input
+        type="text "
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {error.show && <div className="error">{error.msg}</div>}
+    </form>
+  );
+};
+
+export default SearchForm;
